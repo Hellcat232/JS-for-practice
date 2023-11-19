6c3552b7d0070449ba744347836bf98ed172a8ab
Hellcat232 <maxkuich1993@gmail.com>
2023-11-19 17:46:17 +0200

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -1,26 +1,26 @@
-function checkStorage(available, ordered) {
-  //   let check;
-  //   if (available < ordered) {
-  //     check = "Not enough goods in stock!";
-  //   } else {
-  //     check = "Order is processed, our manager will contact you";
-  //   }
-  //   return check;
-  let check = `${(available, ordered)}`;
-  if (available < ordered) {
-    check = "Not enough goods in stock!";
-  } else {
-    check = "Order is processed, our manager will contact you";
-    return check;
-  }
+// function checkStorage(available, ordered) {
+//   let check;
+//   if (available < ordered) {
+//     check = "Not enough goods in stock!";
+//   } else {
+//     check = "Order is processed, our manager will contact you";
+//   }
+//   return check;
+// }
+
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(150, 180));
+// console.log(checkStorage(200, 150));
+// console.log(checkStorage(100, 130));
+
+function getSubstring(string, length) {
+  return string.slice(0, length);
+  // const fullText = string;
+  // return fullText.slice(0, length);
 }
-checkStorage(200, 20);
-checkStorage(100, 50);
-checkStorage(150, 180);
-checkStorage(200, 150);
-checkStorage(100, 130);
-console.log(checkStorage(200, 20));
-console.log(checkStorage(100, 50));
-console.log(checkStorage(150, 180));
-console.log(checkStorage(200, 150));
-console.log(checkStorage(100, 130));
+console.log(getSubstring("Hello world", 3));
+console.log(getSubstring("Hello world", 5));
+console.log(getSubstring("Hello world", 8));
+console.log(getSubstring("Hello world", 11));
+console.log(getSubstring("Hello world", 0));
