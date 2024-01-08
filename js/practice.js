8e60e865643f00b055c8f43573591894eddfb96f
Hellcat232 <maxkuich1993@gmail.com>
2024-01-08 15:54:25 +0200

    dfg

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,13 @@
+const el = document.querySelector(".text");
+const nested = document.querySelector(".sub-text");
+console.log(el.textContent);
+console.log(nested.textContent);
 
+const link = document.querySelector(".link");
+console.log(link);
+const hasActiveClass = link.classList.contains("is-active");
+const hasTitleClass = link.classList.contains("title");
+console.log(hasActiveClass, hasTitleClass);
+link.classList.add("special");
+link.classList.remove("is-active");
+console.log(link.classList);
