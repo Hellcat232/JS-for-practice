afb23d61841599d76794e59cc35c61df5300e066
Hellcat232 <maxkuich1993@gmail.com>
2024-01-10 17:31:51 +0200

    dregt

--- mysripts.js
+++ mysripts.js
@@ -1,4 +1,17 @@
+const body = document.querySelector(".body");
+body.classList.add("body");
+
+//=============================================
 const header = document.querySelector("header");
+const myLogo = document.createElement("a");
+myLogo.classList.add("logo-link");
+header.prepend(myLogo);
+const logoImg = document.createElement("img");
+logoImg.classList.add("logo-img");
+myLogo.prepend(logoImg);
+console.log(myLogo, logoImg);
+const textTitle = document.querySelector(".title-text");
+textTitle.innerHTML = 'New title for <span class = "title-span">JS</span> page';
 //=========================================
 const headerMenu = document.createElement("ul");
 headerMenu.classList.add("menu");
@@ -21,3 +34,15 @@ textElement.textContent =
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime.";
 textHero.append(textElement);
 console.log(textElement.innerHTML);
+//================================================
+const ourTechnologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+const offers = document.querySelector(".our-offers");
+const listOffers = ourTechnologies
+  .map((technology) => `<li class ="offers-item">${technology}</li>`)
+  .join("");
+console.log(listOffers);
+offers.innerHTML = listOffers;
+offers.insertAdjacentHTML(
+  "beforeend",
+  '<li class = "offers-item">TypeScript</li>'
+);
