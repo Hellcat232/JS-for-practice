b05be3b5d5aa9ac24434b17614b257799452a912
Hellcat232 <maxkuich1993@gmail.com>
2024-01-31 00:44:01 +0200

    gv

--- js/practice.js
+++ js/practice.js
@@ -49,10 +49,31 @@
 // heading.textContent = "This is a heading";
 // console.log(heading);
 
-// Функция с параметром
-function приветствиеИмя(имя) {
-  console.log("Привет, " + имя + "!");
-}
+const array = [1, 2, 3, 4, 5];
 
-// Вызов функции с аргументом
-приветствиеИмя("Алиса");
+Array.prototype.calcArr = function (n) {
+  return this.map(function (i) {
+    return i + n;
+  });
+};
+
+console.log(array.calcArr(20));
+
+const isSuccess = false;
+
+// Create promise
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve("Success! Value passed to resolve function");
+    } else {
+      reject("Error! Error passed to reject function");
+    }
+  }, 2000);
+});
+
+promise
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Promise settled"));
+document.r;
